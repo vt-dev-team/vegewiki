399520793678000e3a4b895c461137a7480ba20f
yemaster <yemaster@ahstu.cc>
2020-07-01 13:18:49 +0800

    Init

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a-layout-footer style="text-align: center" v-html="msg">
+    </a-layout-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data () {
+    return {
+      msg: 'Powered by VTDOCS. CopyRight &copy; 1999-2020 <a href="https://github.com/vt-dev-team">vt-dev-team</a>'
+    }
+  }
+}
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        :default-selected-keys="['2']"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">
+          vtdocs
+        </a-menu-item>
+        <a-menu-item key="2">
+          nav 2
+        </a-menu-item>
+        <a-menu-item key="3">
+          nav 3
+        </a-menu-item>
+      </a-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      current: ['mail'],
+      openKeys: ['sub1']
+    }
+  }
+}
+</script>
